8c3669d2501420a6e60052a0bd7a4c54b1581b0a
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-05-26 11:53:37 +0300

    Install react-day-picker package and set up selecting multiple days

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Calendar from './Components/Calendar/Calendar';
 
 function App() {
   return (
     <div>
-      Hello world
+      <Calendar />
     </div>
   );
 }
