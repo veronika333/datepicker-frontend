33edd432f3f7951a993883cb2aaab6594de0fc4e
veronika333 <vera3@inbox.ru>
2020-05-31 19:46:49 +0300

    calendar

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,39 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
-class Calendar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDayClick = this.handleDayClick.bind(this);
-        this.state = {
-            selectedDays: [],
-        };
-    }
+const Calendar = () => {
+const [date, setDate] = useState(new Date());
+  const onDayClick = date => {
+      setDate(date);
+  };
+//   const [days, setSelectedDays] = useState([]);
+//     const selectedDays = (days, selected) => {
+//         setSelectedDays(days);
+//     };
+//     if (selected) {
+//       const selectedIndex = selectedDays.findIndex(selectedDay =>
+//         DateUtils.isSameDay(selectedDay, days)
+//       );
+//       selectedDays.splice(selectedIndex, 1);
+//     } else {
+//       selectedDays.push(days);
+//     }
+    
+    // handleDayClick(day, { selected }) {
+    //     const { selectedDays } = this.state;
+    //     if (selected) {
+    //         const selectedIndex = selectedDays.findIndex(selectedDay =>
+    //             DateUtils.isSameDay(selectedDay, day)
+    //         );
+    //         selectedDays.splice(selectedIndex, 1);
+    //     } else {
+    //         selectedDays.push(day);
+    //     }
+    //     this.setState({ selectedDays });
+    // }
 
-    handleDayClick(day, { selected }) {
-        const { selectedDays } = this.state;
-        if (selected) {
-            const selectedIndex = selectedDays.findIndex(selectedDay =>
-                DateUtils.isSameDay(selectedDay, day)
-            );
-            selectedDays.splice(selectedIndex, 1);
-        } else {
-            selectedDays.push(day);
-        }
-        this.setState({ selectedDays });
-    }
-
-    render() {
+    
         return (
             <div>
                 <DayPicker
-                    selectedDays={this.state.selectedDays}
-                    onDayClick={this.handleDayClick}
+                    onDayClick={onDayClick} value={date}
                 />
+                {console.log(date)}
+                {date.toString()}
             </div>
         );
-    }
+    
 }
 
 export default Calendar;
