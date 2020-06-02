26a41ea16d4dfaf227948c5360b4216e4721dd87
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-06-02 11:22:00 +0300

    add submit handler to login form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,45 +1,55 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [loginUser, setLoginUser] = useState({
+    username: "",
+    password: "",
+  });
 
-    const [loginUser, setLoginUser] = useState({
-        username: '',
-        password: ''
-    })
+  const changeHandler = (e) => {
+    setLoginUser({
+      ...loginUser,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const changeHandler = (e) => {
-        setLoginUser({
-            ...loginUser,
-            [e.target.name]: e.target.value
-        })
-    }
+  // send registration from front to back
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8010/login/send", loginUser)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    // send registration from front to back
-    const submitHandler = (e) => {
-        e.preventDefault();
-        axios.post('http://localhost:8010/login/send', loginUser)
-            .then(response => {
-                console.log(response.data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    };
-
-    return (
-        <div>
-            <form>
-                <label htmlFor="username">
-                    <input type="text" id="username" name="username" onChange={changeHandler}></input>
-                </label>
-                <label htmlFor="password">
-                    <input type="password" id="password" name="passwrod" onChange={changeHandler}></input>
-                </label>
-                <button type="submit" onClick={submitHandler}>Submit</button>
-            </form>
-        </div>
-    );
-}
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="username">
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={changeHandler}
+          ></input>
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            id="password"
+            name="passwrod"
+            onChange={changeHandler}
+          ></input>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
