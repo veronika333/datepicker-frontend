04f364f5d96b9dd009983d91ddfc430cebe1e40f
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-06-02 14:39:40 +0300

    remove duplicate submit handler

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-import axios from 'axios';
+import axios from "axios";
 
 const Login = () => {
-    let history = useHistory();
-    const [loginUser, setLoginUser] = useState({
-        username: '',
-        password: ''
-    })
+  let history = useHistory();
+  const [loginUser, setLoginUser] = useState({
+    username: "",
+    password: "",
+  });
   const changeHandler = (e) => {
     setLoginUser({
       ...loginUser,
@@ -19,53 +19,47 @@ const Login = () => {
   // send registration from front to back
   const submitHandler = (e) => {
     e.preventDefault();
+    console.log(loginUser);
     axios
       .post("http://localhost:8010/login/send", loginUser)
       .then((response) => {
-        console.log(response.data);
+        if (!response.data.error) {
+          console.log("Front: successful signup");
+        } else {
+          console.log("username already taken");
+        }
+        // Redirect
+        history.push("/");
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.log("Front: signup error: ");
+        console.log(error);
       });
   };
 
-    // send registration from front to back
-    const submitHandler = (e) => {
-        e.preventDefault();
-        console.log(loginUser)
-        axios.post('http://localhost:8010/login/send', loginUser)
-            .then((response) => {
-
-                if (!response.data.error) {
-                    console.log('Front: successful signup');
-                } else {
-                    console.log('username already taken')
-                }
-                // Redirect
-                history.push('/');
-            })
-            .catch(error => {
-                console.log('Front: signup error: ')
-                console.log(error)
-            })
-
-    }
-
-
-    return (
-        <div>
-            <form onSubmit={submitHandler} >
-                <label htmlFor="username">
-                    <input type="text" id="username" name="username" onChange={changeHandler}></input>
-                </label>
-                <label htmlFor="password">
-                    <input type="password" id="password" name="password" onChange={changeHandler}></input>
-                </label>
-                <button type="submit" >Submit</button>
-            </form>
-        </div>
-    );
-}
-
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="username">
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={changeHandler}
+          ></input>
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={changeHandler}
+          ></input>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
