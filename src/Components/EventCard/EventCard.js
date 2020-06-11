61f9c990a0ef423631d231de6900cf7a2e49d5cd
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-06-11 13:38:42 +0300

    Remove extra codes

--- src/Components/EventCard/EventCard.js
+++ src/Components/EventCard/EventCard.js
@@ -2,10 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import ReadMore from '../ReadMorePopup/ReadMore';
-import useModal from '../Event/useModal';
 
-const EventCard = ({ title, description, date, link, addLikeHandler, likes }) => {
+const EventCard = ({ title, description, date, link, addLikeHandler, likes, handleShow, deleteHandler }) => {
 
     return (
         <Card style={{ margin: '10px' }} >
@@ -18,32 +16,14 @@ const EventCard = ({ title, description, date, link, addLikeHandler, likes }) =>
                     Read More
       </Link>
                 <p>
-
                     <Button onClick={addLikeHandler} >Like</Button>
                 </p>
 
-const EventCard = ({
-  title,
-  description,
-  date,
-  link,
-  handleShow,
-  deleteHandler,
-}) => {
-  return (
-    <Card style={{ margin: "10px" }}>
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <Card.Text>{date}</Card.Text>
-        <Link to={link} className="linky">
-          Read More
-        </Link>
-        <Button onClick={handleShow}>Delete</Button>
-        <Button onClick={deleteHandler}>Test Delete</Button>
-      </Card.Body>
-    </Card>
-  );
+                <Button onClick={handleShow}>Delete</Button>
+                <Button onClick={deleteHandler}>Test Delete</Button>
+            </Card.Body>
+        </Card>
+    );
 };
 
 export default EventCard;
