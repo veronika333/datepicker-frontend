e16dec4d0a5e5f504a1666df7cca3b63749e025b
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-06-15 10:41:29 +0300

    added title to deletePost

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -11,118 +11,132 @@ import Logout from "../Logout/Logout";
 import DeleteConfirmation from "../Modals/DeleteConfirmation";
 
 const Events = () => {
-    const [post, setPost] = useState([]);
-    let match = useRouteMatch();
-    //Modal for delete
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const [post, setPost] = useState([]);
+  let match = useRouteMatch();
+  //Modal for delete
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    useEffect(() => {
-        axios.get("http://localhost:8010/event")
-            .then((response) => {
-                const posts = response.data;
-                setPost(posts);
-                console.log(posts);
-            });
-    }, []);
-
-    const addLikeHandler = (_id) => {
-        const nextPost = post.find((p) => p._id === _id);
-        console.log(nextPost);
-        const addLike = { ...nextPost };
-        addLike.likes = addLike.likes + 1;
-        
-         //axios.patch(url[, data[, config]])
-        let config = {
-            headers: {
-                "Content-Type": "application/json",
-            },
-        };
-        axios.patch("http://localhost:8010/event/" + _id, addLike, config)
-            .then((response) => {
-                console.log(response)
-                setPost(response.data);
-            })
-            .then(() => {
-                return axios.get("http://localhost:8010/event/");
-            })
-            .catch((err) => {
-                console.log(err)
-            }
-            )
-        window.location.reload(false);
-    }
-
-
-    const deleteHandler = (_id) => {
-        console.log(_id);
-
-        const confirm = window.confirm(`Would you like to delete ${_id} ?`);
-        if (confirm === true) {
-            axios
-                .delete("http://localhost:8010/event/" + _id)
-                .then(() => {
-                    return axios.get("http://localhost:8010/event");
-                })
-                .then((response) => {
-                    //console.log(response.data);
-                    setPost(response.data);
-                });
-        } else {
-            return axios.get("http://localhost:8010/event");
-        }
-        window.location.reload(false);
-    };
-
-    // Update Postlist when the button on NewEventPost.js clicked
-    const updateHandler = () => {
-        window.location.reload(false);
-    };
-
-    const PostList = post.map((p) => {
-        return (
-            <div key={p._id}>
-                <EventCard title={p.title} description={p.description} date={p.date} link={`${match.url}/${p._id}`} likes={p.likes} addLikeHandler={() => addLikeHandler(p._id)} handleShow={handleShow}
-                    deleteHandler={() => deleteHandler(p._id)} />
-
-                <DeleteConfirmation
-                    show={show}
-                    handleClose={handleClose}
-                    deleteHandler={() => deleteHandler(p._id)}
-                />
-            </div>
-        );
+  useEffect(() => {
+    axios.get("http://localhost:8010/event").then((response) => {
+      const posts = response.data;
+      setPost(posts);
+      console.log(posts);
     });
-    return (
-        <>
-            <Logout />
-            <Container>
-                <Row>
-                    <Col sm>
-                        <NewEventPost updateHandler={updateHandler} />
-                    </Col>
+  }, []);
 
-                    <Col>
-                        <Switch>
-                            <Route path="/event/:eventId">
-                                <Event />
-                            </Route>
-                            <Route path={match.path}>
-                                <Col sm
-                                >
-                                    <h2 style={{
-                                        textAlign: "center", margin: "20px"
-                                    }}>LATEST EVENTS</h2>
-                                </Col>
-                                {PostList}
-                            </Route>
-                        </Switch>
-                    </Col>
-                </Row>
-            </Container>
-        </>
+  const addLikeHandler = (_id) => {
+    const nextPost = post.find((p) => p._id === _id);
+    console.log(nextPost);
+    const addLike = { ...nextPost };
+    addLike.likes = addLike.likes + 1;
+
+    //axios.patch(url[, data[, config]])
+    let config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    axios
+      .patch("http://localhost:8010/event/" + _id, addLike, config)
+      .then((response) => {
+        console.log(response);
+        setPost(response.data);
+      })
+      .then(() => {
+        return axios.get("http://localhost:8010/event/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    window.location.reload(false);
+  };
+
+  const deleteHandler = (_id, title) => {
+    const deletePost = post.find((p) => p._id === _id);
+    console.log(_id);
+    console.log(deletePost.title);
+
+    const confirm = window.confirm(
+      `Would you like to delete the event: ${deletePost.title}?`
     );
+    if (confirm === true) {
+      axios
+        .delete("http://localhost:8010/event/" + _id)
+        .then(() => {
+          return axios.get("http://localhost:8010/event");
+        })
+        .then((response) => {
+          //console.log(response.data);
+          setPost(response.data);
+        });
+    } else {
+      return axios.get("http://localhost:8010/event");
+    }
+    window.location.reload(false);
+  };
+
+  // Update Postlist when the button on NewEventPost.js clicked
+  const updateHandler = () => {
+    window.location.reload(false);
+  };
+
+  const PostList = post.map((p) => {
+    return (
+      <div key={p._id}>
+        <EventCard
+          title={p.title}
+          description={p.description}
+          date={p.date}
+          link={`${match.url}/${p._id}`}
+          likes={p.likes}
+          addLikeHandler={() => addLikeHandler(p._id)}
+          handleShow={handleShow}
+          deleteHandler={() => deleteHandler(p._id, p.title)}
+        />
+
+        <DeleteConfirmation
+          show={show}
+          handleClose={handleClose}
+          deleteHandler={() => deleteHandler(p._id)}
+        />
+      </div>
+    );
+  });
+  return (
+    <>
+      <Logout />
+      <Container>
+        <Row>
+          <Col sm>
+            <NewEventPost updateHandler={updateHandler} />
+          </Col>
+
+          <Col>
+            <Switch>
+              <Route path="/event/:eventId">
+                <Event />
+              </Route>
+              <Route path={match.path}>
+                <Col sm>
+                  <h2
+                    style={{
+                      textAlign: "center",
+                      margin: "20px",
+                    }}
+                  >
+                    LATEST EVENTS
+                  </h2>
+                </Col>
+                {PostList}
+              </Route>
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Events;
